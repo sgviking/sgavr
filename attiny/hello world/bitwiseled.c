b022bc5312e8a5e74a47c3fd349bde72af9c1bc1
Dan Daggett <socialgeek@gmail.com>
2012-08-25 22:07:12 -0600

    Added some more comments

--- attiny/hello world/bitwiseled.c
+++ attiny/hello world/bitwiseled.c
@@ -33,24 +33,20 @@ int main(void)
 			_delay_ms(150);
 		}*/
 
-		// Twiddle (flip) some bits
 		pin_pattern = 0b00010101;
 
 		PORTB = pin_pattern;
 		_delay_ms(250);
+		// twiddle my bits
 		PORTB = ~pin_pattern;
 		_delay_ms(250);
 		PORTB = pin_pattern;
 		_delay_ms(250);
+		// do you twiddle your bits?
 		PORTB = ~pin_pattern;
 		_delay_ms(150);
 	
-		pin_pattern = 0b00000001;
-		
-		//asm volatile 
-		//(
-		//	"rol %0\n\t" : "=d" (pin_pattern) : "0" (pin_pattern)
-		//);
+		//pin_pattern = 0b00000001;
 	}
 	
 	return 1;
