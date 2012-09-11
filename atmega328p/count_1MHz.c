91dda0a09f12c202b5c7b653fbe3488851005cf6
en0 <en0@mail.com>
2012-09-11 09:09:49 -0600

    Added code to demonstrate interrupts.

--- atmega328p/count_1MHz.c
+++ atmega328p/count_1MHz.c
@@ -14,16 +14,38 @@
  * define it here.
  */
 
-#ifndef F_CPU
-
 // Define for 1MHz clock cycle.
+#ifndef F_CPU
 #define F_CPU 1000000UL
-
 #endif
+
+// Utilties for configuring interrupts.
+#define RSNG_EDG 0b011
+#define SET_INTTYPE(ctrlr,n,f) (ctrlr |= (f << (n*2)))
+#define SET_INTERRUPT(msk,n) (msk |= (1 << n))
+
+#define DELAY_MIN 1
+#define DELAY_MAX 20
+#define CLOCK_DELAY 10
+
+/** Global Includes **/
 
 #include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+
+/** Global Variables **/
+
+/*
+ * Volatile Storage for main loop delay.
+ * Volatile will keep GCC from optimizing away
+ * our shared memory.
+ */
+
+volatile uint_fast8_t __dc = DELAY_MAX / 2;
+
 
 /** PROTOTYPES **/
 
@@ -32,6 +54,14 @@
  */
 
 void ioinit(void);
+
+/*
+ * Initialize hardware interrupts.
+ * This function does NOT set the global
+ * interrupt flag.
+ */
+
+void isrinit(void);
 
 /*
  * Entry point containing main loop.
@@ -48,13 +78,26 @@ int main (void)
   PORTB = 0x00;
   PORTD = 0x00;
 
+  // Initialize and enable hardware interrupts.
+
+  isrinit();
+  sei();
 
   // Loop through all values of i and
   // set on PortC.
 
-  uint_fast8_t i;
+  uint_fast8_t i, dc;
   for(i = 0; ; PORTC = i++ & 0x0F) {
-    _delay_ms(250);
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+      dc = __dc;
+    }
+
+    do {
+      _delay_ms(CLOCK_DELAY);
+    } while(dc--);
+
   }
 
   return 0;
@@ -73,4 +116,34 @@ void ioinit (void)
   
   // RXD for serial read on PD0 is input
   DDRD = 0b11111110;
+}
+
+
+/*
+ * Initialize hardware interrupts.
+ * This function does NOT set the global
+ * interrupt flag.
+ */
+
+void isrinit(void)
+{
+  // Set ISR 0 and 1 to trigger on rising edge.
+  SET_INTTYPE(EICRA, 0, RSNG_EDG);
+  SET_INTTYPE(EICRA, 1, RSNG_EDG);
+
+  // Enable ISR 0 and 1.
+  SET_INTERRUPT(EIMSK, 0);
+  SET_INTERRUPT(EIMSK, 1);
+}
+
+ISR(INT0_vect)
+{
+  if(__dc < DELAY_MAX)
+    __dc++;
+}
+
+ISR(INT1_vect)
+{
+  if(__dc > DELAY_MIN)
+    __dc--;
 }
